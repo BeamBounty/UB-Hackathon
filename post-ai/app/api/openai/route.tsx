88495ce09fc6c4e1002ef4f246051f68265d470f
Matt C <matthewchaves84@gmail.com>
2023-04-15 13:39:13 -0400

    4/15/2023 [Changes] - Added ChatGPT functionality in Route.tsx

--- post-ai/app/api/openai/route.tsx
+++ post-ai/app/api/openai/route.tsx
@@ -1,25 +1,42 @@
 import { NextResponse } from "next/server"
-
+import {Configuration, OpenAIApi} from "openai"
 // process.env.OPENAI_KEY
 
 type GPTData = {
-	name: string
-	bio: string
-	context: string
+	name: string,
+	bio: string,
+	context: string,
 	tone: string
 	target_demo: string
 }
 
 export async function POST(request: Request) {
 	const req: GPTData = await request.json()
-
 	const prompt = `Company Name: ${req.name}\nCompany Description: ${req.bio}\nGenerate a ${req.tone} tweet geared towards ${req.target_demo} for this company with the following context:\n${req.context}`
-	// use the openai api to prompt chatgpt for a tweet.
-	// feel free to modify the promp if you want.
 
-	const tweet = "Will generate with ChatGPT"
+	//Configure a new OpenAI completion request 
+	const configuration = new Configuration
+	({
+		apiKey : process.env.OPENAI_KEY,
+	});
+
+	const openai = new OpenAIApi(configuration);
+
+	const model_engine = "text-davinci-003";
+	const response = await openai.createCompletion({
+		model: model_engine,
+		prompt: prompt,
+		max_tokens: 64, // Length of response
+		n: 1, // Number of responses
+		stop: null, // Can set a certain string of chars where, if reached, prompt will stop
+		temperature: 1, // How risky will the answers be
+	})
+	const data = response.data;
+	const tweet = data.choices[0];
+	console.log(tweet.text);
+	
 	return NextResponse.json({
-		text: tweet,
+		text: tweet.text,
 		prompt: prompt,
 	})
 }
